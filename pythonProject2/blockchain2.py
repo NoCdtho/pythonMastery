2ae4a3ee756ba305d2c6b6b78a8b74077c29e1a8
Rohit Gupta <12345678+NoCdtho@users.noreply.github.com>
2026-06-19 23:53:04 +0530

    Readme update

--- pythonProject2/blockchain2.py
+++ pythonProject2/blockchain2.py
@@ -1,2 +1,3 @@
 blockchain = [{'data': 'genesis block', 'prev_hash': '0'}]
-blockchain.append({'data: 'Block1', 'prev_hash': hashlib.sha256(str(blockchain[-1]).encode()).hexdigest()})
+blockchain.append({data: 'Block1', 
+                   'prev_hash': hashlib.sha256(str(blockchain[-1]).encode()).hexdigest()})
